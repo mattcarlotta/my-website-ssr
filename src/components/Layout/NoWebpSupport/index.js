4a0df2da91181d26d2fa7ede833b93a953dab791
Matt <carlotta.matt@gmail.com>
2020-08-16 12:44:58 -0700

    Fix background image

--- src/components/Layout/NoWebpSupport/index.js
+++ src/components/Layout/NoWebpSupport/index.js
@@ -5,9 +5,11 @@ const NoWebpSupport = () => {
     const img = new Image();
     img.src =
       "data:image/webp;base64,UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA";
-    img.onerror = () => {
-      document.body.style.background =
-        "url('bg.png') center center no-repeat fixed";
+    img.onload = () => {
+      document.body.style.background = "url('bg.png')";
+      document.body.style.backgroundRepeat = "no-repeat";
+      document.body.style.backgroundAttachment = "fixed";
+      document.body.style.backgroundPosition = "center";
     };
   }, []);
 
--- src/styles/globalStylesheet.js
+++ src/styles/globalStylesheet.js
@@ -17,7 +17,10 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     font-family: "Jura Medium", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-		background: url("./bg.webp") center center no-repeat fixed;
+		background: url("./bg.webp");
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-position: center;
 		background-color: #010007;
 		color: #fff;
   }
