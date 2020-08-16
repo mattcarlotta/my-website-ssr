08717a7b0a325cc14c99c6d7a07982f525860cc1
Matt <carlotta.matt@gmail.com>
2020-08-16 12:55:23 -0700

    Fix background image

--- src/components/Layout/NoWebpSupport/index.js
+++ src/components/Layout/NoWebpSupport/index.js
@@ -5,7 +5,7 @@ const NoWebpSupport = () => {
     const img = new Image();
     img.src =
       "data:image/webp;base64,UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA";
-    img.onload = () => {
+    img.onerror = () => {
       document.body.style.background = "url('bg.png')";
       document.body.style.backgroundRepeat = "no-repeat";
       document.body.style.backgroundAttachment = "fixed";
